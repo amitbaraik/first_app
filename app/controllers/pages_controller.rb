4ec545b07a89a491748ab9bb053b81a90dd1574e
Amit Kumar <amitbaraik@gmail.com>
2010-11-04 19:08:52 +0530

    help added

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,51 +1,15 @@
-
-
-describe PagesController do
-  render_views
-
-  before(:each) do
-    #
-    # Define @base_title here.
-    #
+class PagesController < ApplicationController
+ def home
+    @title = "Home"
   end
 
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                                    :content => @base_title + " | Home")
-    end
+  def contact
+    @title = "Contact"
   end
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                                    :content => @base_title + " | Contact")
-    end
+  def about
+    @title = "About"
   end
 
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
 
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-                                    :content => @base_title + " | About")
-    end
-  end
 end
-
