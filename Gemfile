797a92387607526f9f59d95834752738a43449ea
Amit Kumar <amitbaraik@gmail.com>
2010-11-10 12:11:45 +0530

    gravtar

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'rails', '3.0.0'
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 gem 'sqlite3-ruby', :require => 'sqlite3'
-
+gem 'gravatar_image_tag'
 # Use unicorn as the web server
 # gem 'unicorn'
 
@@ -36,5 +36,6 @@ end
 group :test do
   gem 'rspec'
   gem 'webrat'
+  gem 'factory_girl_rails'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101104115735) do
+ActiveRecord::Schema.define(:version => 20101110062046) do
 
   create_table "microposts", :force => true do |t|
-    t.string   "content"
-    t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "content"
+    t.integer   "user_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "name"
+    t.string    "email"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "encrypted_password"
+    t.string    "salt"
   end
 
 end
